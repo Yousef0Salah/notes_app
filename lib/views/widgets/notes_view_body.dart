79cbc0ea6f3de82f9c428399e292ad74edb629ff
Yousef0Salah <zoom28644@gmail.com>
2025-11-03 23:22:53 +0200

    custom app bar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notes/views/widgets/custom_app_bar.dart';
+
+class NotesViewsBody extends StatelessWidget {
+  const NotesViewsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(children: const [SizedBox(height: 50), CustomAppBar()]),
+    );
+  }
+}
